53ad4a4c8110bc0d4e03a8a7702eb9a9419b2f5e
Joao Marcos <joao.marcos.witcel@gmail.com>
2025-08-02 15:15:02 -0300

    WIP: type infer

--- src/csv.cpp
+++ src/csv.cpp
@@ -12,13 +12,46 @@
 // @note João, considerar usar cstrings e array estáticos para o header e as linhas
 using CSV_Data_Row = std::vector<std::string>;
 
+enum Data_Cell_Type
+{
+  TEXT,
+  // @note João, adicionar distinção entre inteiros, floats e pelo tamanho dos números?
+  NUMBER,
+};
+
+struct Data_Cell_Type_Info
+{
+  Data_Cell_Type type;
+  bool nullable;
+};
+
 struct CSVData
 {
   // @todo João, modelar uma coluna dedicada a metadados sobre o parsing e os tipos inferidos
   // das colunas. Armazenar se houve algum erro em alguma linha e talvez traquear as linhas problemáticas
   // para remoção ou tratamento
   CSV_Data_Row header;
+  std::vector<Data_Cell_Type_Info> infered_types_for_columns;
   std::vector<CSV_Data_Row> dataset;
+
+  void infer_types()
+  {
+    if (!this->header.size()) return;
+    if (!this->dataset.size()) return;
+
+    for (size_t i = 0; i < this->header.size(); i++)
+    {
+      Data_Cell_Type_Info col_info = { .type = NUMBER, .nullable = false, };
+
+      for (const auto &row : this->dataset)
+      {
+        const auto &dataField = row[i];
+        // @todo João, terminar aqui...
+
+
+      }
+    }
+  }
 };
 
 constexpr int32_t END_OF_SOURCE = -9999;
